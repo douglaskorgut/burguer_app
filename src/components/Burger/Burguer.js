93247703a7b5c7de635e9447cabe824785fa5a2d
douglaskorgut <douglas.korgut@ubivis.io>
2020-04-11 20:49:58 -0300

    Burger ingredients being created dinamically (props from burger builder class)

--- src/components/Burger/Burguer.js
+++ src/components/Burger/Burguer.js
@@ -4,11 +4,21 @@ import Aux from '../../hoc/Aux'
 import classes from './Burguer.module.css'
 
 const burger = (props) =>{
+
+        // Convert object to key value pairs
+        const transformedIngredients = Object.keys(props.ingredients)
+
+            // Convert key-value pair in burger ingredients
+            .map(igKey => {
+                    return [...Array(props.ingredients[igKey])].map((_,index) => {
+                            return <BurgerIngredient key={igKey + index} type={igKey} />
+                    });
+            });
+
         return (
             <div className={classes.Burger}>
                 <BurgerIngredient type='bread-top'/>
-                <BurgerIngredient type='cheese'/>
-                <BurgerIngredient type='meat'/>
+                    {transformedIngredients}
                 <BurgerIngredient type='bread-bottom'/>
             </div>
         )
